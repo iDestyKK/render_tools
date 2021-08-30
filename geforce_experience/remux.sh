4292826f1aeafaac4c7f37e206980c1d7e80eaf9
iDestyKK <clara@claranguyen.me>
2021-08-29 20:32:03 -0400

    Added 16 channel audio and 7.1 audio generation support from dev/hdr_gameplay

--- geforce_experience/remux.sh
+++ geforce_experience/remux.sh
@@ -27,6 +27,10 @@ PARAM_FFMPEG=""
 # WavPack Parameters
 PARAM_WAVPACK="-hhx4m"
 
+# Can we even use WavPack?
+command -v wavpack > /dev/null
+WV_ABLE=$?
+
 # -----------------------------------------------------------------------------
 # Script Variable Setup                                                    {{{1
 # -----------------------------------------------------------------------------
@@ -68,6 +72,8 @@ for F in *.mp4; do
 	AMD=""   # Audio Metadata
 	MD=""    # Misc Metadata (unrelated to streams)
 	MAP=""   # Mapping Information
+	AMP=0    # Amplification Amount
+	FILT=""  # FFmpeg "filter_complex", if needed
 
 	# The Video File is obviously the video stream
 	I_STR="-i \"$F\""
@@ -79,6 +85,164 @@ for F in *.mp4; do
 	CMP="$CMP -c:v:$VI copy"
 
 	let "VI++"
+	let "FI++"
+
+	#
+	# If a "(16ch).raw" or "(16ch).wav" file exists. Compress it via WavPack
+	# and remux into an "(16ch).mka". If WavPack isn't a valid command,
+	# TrueAudio (TTA) is used instead. If this file already exists, we can skip
+	# the step. The 16 channel Dolby Atmos master can then be used to generate
+	# a 7.1 Surround Sound file which is more compatible with video players and
+	# YouTube.
+	#
+
+	if [ -e "$BF (16ch).wv" ]; then
+
+		# A pre-compressed WV has been delivered. Remux it.
+		ffmpeg -i "$BF (16ch).wv" -map 0:a -c copy "__AUDIO_tmp_16ch.mka"
+
+	elif [ -e "$BF (16ch).tta" ]; then
+
+		# A pre-compressed TTA has been delivered. Remux it.
+		ffmpeg -i "$BF (16ch).tta" -map 0:a -c copy "__AUDIO_tmp_16ch.mka"
+
+	elif [ -e "$BF (16ch).raw" ]; then
+
+		#
+		# A 16 channel RAW file has been delivered. Assume 48,000hz,
+		# 24 bit, 16 channels.
+		#
+
+		if [ $WV_ABLE -eq 0 ]; then
+			# We can use WavPack
+			wavpack \
+				$PARAM_FFMPEG --raw-pcm=48000,24s,16,le "$BF (16ch).raw"
+
+			# Remux into MKA container
+			ffmpeg \
+				-i "$BF (16ch).wv" \
+				-map 0:a \
+				-c copy \
+				"__AUDIO_tmp_16ch.mka"
+
+			# Clean up
+			rm -f "$BF (16ch).wv"
+		else
+			# We can't use WavPack. Instead, use TrueAudio (TTA) via FFmpeg
+			ffmpeg \
+				-f s24le -ar 48000 -ac 16 \
+				-i "$BF (16ch).raw" \
+				-map 0:a \
+				-c tta \
+				"__AUDIO_tmp_16ch.mka"
+		fi
+
+	elif [ -e "$BF (16ch).wav" ]; then
+
+		# A 16 channel WAV file has been delivered instead.
+		if [ $WV_ABLE -eq 0 ]; then
+			# We can use WavPack
+			wavpack $PARAM_FFMPEG "$BF (16ch).wav"
+
+			# Remux into MKA container
+			ffmpeg \
+				-i "$BF (16ch).wv" \
+				-map 0:a \
+				-c copy \
+				"__AUDIO_tmp_16ch.mka"
+
+			# Clean up
+			rm -f "$BF (16ch).wv"
+		else
+			# We can't use WavPack. Instead, use TrueAudio (TTA) via FFmpeg
+			ffmpeg \
+				-i "$BF (16ch).wav" \
+				-map 0:a \
+				-c tta \
+				"__AUDIO_tmp_16ch.mka"
+		fi
+
+	fi
+
+	#
+	# Check if a "__AUDIO_tmp_16ch.mka" has been created fropm the disaster
+	# above. If so, generate a 7.1 Surround Sound track from it.
+	#
+
+	if [ -e "__AUDIO_tmp_16ch.mka" ]; then
+		#
+		# A 16 channel (7.1.4.4) audio track was provided. Ensure that the
+		# first track is a 7.1 track via generating it from the 16 channel
+		# audio. A 7.1 mix can be generated via "flattening" the original mix
+		# into 8 channels via:
+		#
+		#     FL  = FL + (TFL / 2) + (BFL / 2)
+		#     FR  = FR + (TFR / 2) + (BFR / 2)
+		#     FC  = FC + (TFL / 4) + (TFR / 4) + (BFL / 4) + (BFR / 4)
+		#     LFE = LFE
+		#     BL  = BL + (TBL / 2) + (BBL / 2)
+		#     BR  = BR + (TBR / 2) + (BBR / 2)
+		#     SL  = SL + (TBL / 2) + (BBL / 2) + (TFL / 4) + (BFL / 4)
+		#     SR  = SR + (TBR / 2) + (BBR / 2) + (TFR / 4) + (BFR / 4)
+		#
+
+		C0="c0=c0 + 0.5 * c8 + 0.5 * c12"
+		C1="c1=c1 + 0.5 * c9 + 0.5 * c13"
+		C2="c2=c2 + 0.25 * c8 + 0.25 * c12 + 0.25 * c9 + 0.25 * c13"
+		C3="c3=c3"
+		C4="c4=c4 + 0.5 * c10 + 0.5 * c14"
+		C5="c5=c5 + 0.5 * c11 + 0.5 * c15"
+		C6="c6=c6 + 0.5 * c10 + 0.5 * c14 + 0.25 * c8 + 0.25 * c12"
+		C7="c7=c7 + 0.5 * c11 + 0.5 * c15 + 0.25 * c9 + 0.25 * c13"
+
+		CH_MAP="$C0|$C1|$C2|$C3|$C4|$C5|$C6|$C7"
+
+		ffmpeg \
+			-i "__AUDIO_tmp_16ch.mka" \
+			-filter_complex "[0:a]pan=7.1|$CH_MAP[a]" \
+			-map '[a]' \
+			-c:a pcm_s24le \
+			"__AUDIO_tmp_7.1ch.wav"
+
+		# Have FFmpeg take a look at the amplification needed to make it LOUD
+		AMP=$(
+			ffmpeg \
+				-i "__AUDIO_tmp_7.1ch.wav" \
+				-af "volumedetect" \
+				-f null NUL \
+				2>&1 \
+				| grep "max_volume" \
+				| sed 's/.*max_volume: -\?\(.*\) dB/\1/'
+		)
+
+		#
+		# Since a 7.1ch and a 16ch track is guaranteed to exist, go on and add
+		# the input and metadata information necessary.
+		#
+
+		# 7.1ch information
+		I_STR="$I_STR -i __AUDIO_tmp_7.1ch.wav"
+		FILT="-filter_complex \"[$FI:a:0]volume=${AMP}dB[amped]\""
+		MAP="$MAP -map \"[amped]\""
+		CMP="$CMP -c:a:$AI flac -compression_level 12"
+
+		AMD="$AMD -metadata:s:a:$AI title=\"Game Audio [7.1 Surround]\""
+		AMD="$AMD -metadata:s:a:$AI language=\"$GA_LANG\""
+
+		let "AI++"
+		let "FI++"
+
+		# 16ch information
+		I_STR="$I_STR -i __AUDIO_tmp_16ch.mka"
+		MAP="$MAP -map $FI:a:0"
+		CMP="$CMP -c:a:$AI copy"
+
+		AMD="$AMD -metadata:s:a:$AI title=\"Game Audio [7.1.4.4 Master]\""
+		AMD="$AMD -metadata:s:a:$AI language=\"$GA_LANG\""
+
+		let "AI++"
+		let "FI++"
+	fi
 
 	#
 	# If no "stX (Game Audio).*" exists, and if no "(16ch).raw" or "(16ch).wv"
@@ -96,15 +260,12 @@ for F in *.mp4; do
 	RES_2=$?
 
 	#if [ $RES_1 -ne 0 ] && [ $RES_2 -ne 0 ]; then
-		MAP="$MAP -map $FI:a:0"
+		MAP="$MAP -map 0:a:0"
 		AMD="$AMD -metadata:s:a:$AI title=\"Game Audio [GeForce Experience]\""
 		AMD="$AMD -metadata:s:a:$AI language=\"$GA_LANG\""
 		CMP="$CMP -c:a:$AI copy"
 		let "AI++"
 	#fi
-
-	# We're done with the video file.
-	let "FI++"
 
 	#
 	# Go through all st0, st1, st2, ..., stN files and append them as valid
@@ -158,6 +319,10 @@ for F in *.mp4; do
 
 	MD="$MD -metadata DATE_RECORDED=\"$DATE_REC\""
 	MD="$MD -metadata DATE_ENCODED=\"$DATE_ENC\""
+	MD="$MD -disposition none"
 
-	eval "ffmpeg $FFMPEG_PARAM $I_STR $MAP $CMP $VMD $AMD $MD \"$OUT\""
+	eval "ffmpeg $FFMPEG_PARAM $I_STR $FILT $MAP $CMP $VMD $AMD $MD \"$OUT\""
+
+	# Clean up
+	rm -f "__AUDIO_tmp_7.1ch.wav" "__AUDIO_tmp_16ch.mka"
 done
